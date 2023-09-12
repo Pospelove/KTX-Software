ba8b1303feef485af687909448a2856a30271145
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-09-12 15:30:00 +0600

    vanila

--- utils/stdafx.h
+++ utils/stdafx.h
@@ -8,18 +8,7 @@
 #define _CRT_SECURE_NO_WARNINGS // For _WIN32. Must be before <stdio.h>.
 #include <assert.h>
 #include <stdio.h>
-
-#ifdef UNICODE
-#undef UNICODE
-#endif
-
-#if WIN32
-  #if _MSC_VER < 1900
-    #define snprintf _snprintf
-  #endif
-#endif
-
-#if 0
+#ifdef _WIN32
   #include <io.h>
   #include <tchar.h>
   #if _MSC_VER < 1900
